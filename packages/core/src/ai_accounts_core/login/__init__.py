dbe1d1db2264b4bd2df5f73ae942a1fbf16b9b00
Cameleon X <ca1773130n@gmail.com>
2026-04-11 23:23:14 +0900

    feat(core): LoginEvent discriminated union + PromptAnswer

---
+++ packages/core/src/ai_accounts_core/login/__init__.py
@@ -0,0 +1,21 @@
+from ai_accounts_core.login.events import (
+    LoginComplete,
+    LoginEvent,
+    LoginFailed,
+    ProgressUpdate,
+    PromptAnswer,
+    StdoutChunk,
+    TextPrompt,
+    UrlPrompt,
+)
+
+__all__ = [
+    "LoginComplete",
+    "LoginEvent",
+    "LoginFailed",
+    "ProgressUpdate",
+    "PromptAnswer",
+    "StdoutChunk",
+    "TextPrompt",
+    "UrlPrompt",
+]
