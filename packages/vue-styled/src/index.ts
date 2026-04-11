937c16d7d925ee95ef92362e3b53c6bd3d51d863
Cameleon X <ca1773130n@gmail.com>
2026-04-12 00:38:02 +0900

    feat(vue-styled): LoginStream component

--- packages/vue-styled/src/index.ts
+++ packages/vue-styled/src/index.ts
@@ -2,6 +2,7 @@ import './styles/tokens.css';
 
 export { default as AccountWizard } from './components/AccountWizard.vue';
 export { default as OnboardingFlow } from './components/OnboardingFlow.vue';
+export { default as LoginStream } from './components/LoginStream.vue';
 
 export const version = '0.2.0';
 
