b3a097451aa40ade9c44790d240c8604cdcd459f
Cameleon X <ca1773130n@gmail.com>
2026-04-11 23:35:22 +0900

    feat(core): BackendMetadata + supporting Structs

---
+++ packages/core/src/ai_accounts_core/metadata/__init__.py
@@ -0,0 +1,15 @@
+from ai_accounts_core.metadata.types import (
+    BackendMetadata,
+    InputSpec,
+    InstallCheck,
+    LoginFlowSpec,
+    PlanOption,
+)
+
+__all__ = [
+    "BackendMetadata",
+    "InputSpec",
+    "InstallCheck",
+    "LoginFlowSpec",
+    "PlanOption",
+]
